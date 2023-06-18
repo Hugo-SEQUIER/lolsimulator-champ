5d7cdcda5d527937187a436e5b69a931c64fb0cb
sequierh <hugo.sequier@etu.umontpellier.fr>
2023-06-18 22:57:48 +0200

    ajout

--- pages/components/characterDetails.js
+++ pages/components/characterDetails.js
@@ -61,6 +61,63 @@ export default function CharacterDetails({data, nameChamp}){
         [textMana + " / Regen"]: 0,
     })
 
+        function getNumericFromString(stringDamage, passiveDmg){
+
+        // VOIR LES PLAGES NOMMEES
+        let MOD_Phys = 72 / 100
+        let MOD_Hit = 72 / 100
+        let MOD_Magic = 76 / 100
+        let MOD_Heal = 1
+        let MOD_SelfHeal = 1
+
+        let Self_Level = passiveSkillPoint
+        let Sc_Lin = ((Self_Level -1) * 6)/100
+        let Self_AP = additionnalStats["Ability Power"]
+        let Self_AD = statsChamp["Attack Damage"]
+        let Self_BoAD = additionnalStats["Attack Damage"]
+        let Self_MHP = statsChamp["Hp"] + additionnalStats["Hp"]
+        let Self_BoHp = additionnalStats["Hp"]
+        let Self_Crit = additionnalStats["Critical %"] /100
+        let Self_CritDMG = 175 / 100
+        let Self_BoAS = additionnalStats["Attack Speed %"] / 100
+        let E_MHP = statsChamp["Hp"]
+        let E_CHP = 100
+        
+        let P_Q = qSkillPoint
+        let P_W = wSkillPoint
+        let P_E = eSkillPoint
+        let P_R = rSkillPoint
+
+        let Y6 = passiveDmg
+        let Minion = 0
+        let Gametime = 0
+        let IT_CDMG = itemStats["CDMG"]
+        let IT_Proc_Energy = itemStats["Proc_Energy"]
+        let IT_AD = addiitemStatstionnalStats["AD"]
+        let IT_AH = itemStats["AH"]
+        let IT_AP = itemStats["AP"]
+        let IT_APEN = itemStats["APE%"] / 100
+        let IT_AR = itemStats["AR"]
+        let IT_AS = itemStats["AS"] / 100
+        let IT_Crit = itemStats["CC"] / 100
+        let IT_FlatMS = itemStats["MS"]
+        let IT_HP = itemStats["HP"]
+        let IT_HPR = itemStats["HPR"]
+        let IT_Leth = itemStats["Leth"]
+        let IT_LS = itemStats["LS"]
+        let IT_MOD_Heal = itemStats["HEAL"]
+        let IT_MOD_Magic = itemStats["APM"]
+        let IT_MOD_Phys = itemStats["ADM"]
+        let IT_MP = itemStats["MP"]
+        let IT_Mpen = itemStats["MPE"]
+        let IT_MPenP = itemStats["MPE%"] / 100
+        let IT_MPR = itemStats["MPR"] 
+        let IT_MR = itemStats["MR"] 
+        let IT_MS = itemStats["MS"]
+        let IT_OH_Magic = itemStats["OH_Magic"]
+        let IT_OH_Phys = itemStats["OH_Phys"]
+        let IT_OH_True = itemStats["OH_True"]
+    }
     useEffect(()=> {
         if (data != undefined) {
             if (data["Energy"] === "TRUE"){
--- pages/components/skillsTable.js
+++ pages/components/skillsTable.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import math, { add, max } from "mathjs";
 
-function SkillsTable({ statsChamp, statsSpell, passiveSkillPoint, additionnalStats }) {
+function SkillsTable({ itemStats, statsChamp, statsSpell, passiveSkillPoint, additionnalStats }) {
 
     const [qSkillPoint, setQSkillPoint] = useState(1)
     const [wSkillPoint, setWSkillPoint] = useState(1)
@@ -77,9 +77,9 @@ function SkillsTable({ statsChamp, statsSpell, passiveSkillPoint, additionnalSta
         let Self_BoAD = additionnalStats["Attack Damage"]
         let Self_MHP = statsChamp["Hp"] + additionnalStats["Hp"]
         let Self_BoHp = additionnalStats["Hp"]
-        let Self_Crit = additionnalStats["Critical %"] / 100
+        let Self_Crit = additionnalStats["Critical %"] /100
         let Self_CritDMG = 175 / 100
-        let Self_BoAS = additionnalStats["Attack Speed %"]
+        let Self_BoAS = additionnalStats["Attack Speed %"] / 100
         let E_MHP = statsChamp["Hp"]
         let E_CHP = 100
         
@@ -89,13 +89,34 @@ function SkillsTable({ statsChamp, statsSpell, passiveSkillPoint, additionnalSta
         let P_R = rSkillPoint
 
         let Y6 = passiveDmg
-        let OH_Magic = 0
-        let OH_Phys = 0
-        let OH_True = 0
         let Minion = 0
         let Gametime = 0
-        let IT_CDMG = 0
-
+        let IT_CDMG = itemStats["CDMG"]
+        let IT_Proc_Energy = itemStats["Proc_Energy"]
+        let IT_AD = addiitemStatstionnalStats["AD"]
+        let IT_AH = itemStats["AH"]
+        let IT_AP = itemStats["AP"]
+        let IT_APEN = itemStats["APE%"] / 100
+        let IT_AR = itemStats["AR"]
+        let IT_AS = itemStats["AS"] / 100
+        let IT_Crit = itemStats["CC"] / 100
+        let IT_FlatMS = itemStats["MS"]
+        let IT_HP = itemStats["HP"]
+        let IT_HPR = itemStats["HPR"]
+        let IT_Leth = itemStats["Leth"]
+        let IT_LS = itemStats["LS"]
+        let IT_MOD_Heal = itemStats["HEAL"]
+        let IT_MOD_Magic = itemStats["APM"]
+        let IT_MOD_Phys = itemStats["ADM"]
+        let IT_MP = itemStats["MP"]
+        let IT_Mpen = itemStats["MPE"]
+        let IT_MPenP = itemStats["MPE%"] / 100
+        let IT_MPR = itemStats["MPR"] 
+        let IT_MR = itemStats["MR"] 
+        let IT_MS = itemStats["MS"]
+        let IT_OH_Magic = itemStats["OH_Magic"]
+        let IT_OH_Phys = itemStats["OH_Phys"]
+        let IT_OH_True = itemStats["OH_True"]
     }
     return (
         <div className="stats-table skills-table">
