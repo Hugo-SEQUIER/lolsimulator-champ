2f99f8b3ddcd7d0883800498afdafc5b47e149b4
sequierh <hugo.sequier@etu.umontpellier.fr>
2023-07-27 22:59:23 +0200

    Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { Analytics } from "@vercel/analytics/react";
 class MyDocument extends Document {
   render() {
     return (
@@ -32,6 +32,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     );
